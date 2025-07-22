7a4e91e9399d2c8725c4c7cd83f35047d262adaa
Mark20026 <bogimark2002@gmail.com>
2025-07-22 19:43:07 +0200

    Created login and signup

---
+++ src/components/Inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProfilePhotoSelector = ({ image, setImage }) => {
+    const inputRef = useRef(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+  return <div>ProfilePic</div>;
+};
+
+export default ProfilePhotoSelector;
