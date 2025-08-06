b7670035b4025faab90a040b65d2671441a75291
Mark20026 <bogimark2002@gmail.com>
2025-08-06 14:26:08 +0200

    Integrated the SignUp API

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import AuthLayout from "./../../components/layouts/AuthLayout";
 import { useNavigate, Link } from "react-router-dom";
 import Input from "./../../components/Inputs/Input";
 import { validateEmail } from "./../../utils/helper.js";
 import ProfilePhotoSelector from "./../../components/Inputs/ProfilePhotoSelector";
+import axiosInstance from "../../utils/axiosInstance.js";
+import { API_PATHS } from "../../utils/apiPaths.js";
+import { UserContext } from "./../../context/userContext";
+import uploadImage from "./../../utils/uploadImage.js";
 
 const Signup = () => {
   const [profilePic, setProfilePic] = useState(null);
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { updateUser } = useContext(UserContext);
 
   const [error, setError] = useState(null);
 
@@ -37,6 +43,36 @@ const Signup = () => {
     }
 
     setError("");
+
+    //Signup API Call
+    try {
+      //Upload image if present
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
   };
 
   return (
